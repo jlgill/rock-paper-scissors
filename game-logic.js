713d9f883dcf871fd616f731d54ece43e4388f25
James Gill <mcdeth187@gmail.com>
2024-03-21 18:59:12 -0700

    with easter egg

--- game-logic.js
+++ game-logic.js
@@ -1,63 +1,63 @@
+//Prompt user for selection
 let choice = prompt("Please enter your choice here");
+
+//convert selection to lowercase
 let playerChoice = choice.toLowerCase();
 
 function getPlayerChoice() {
-    switch (playerChoice) {
-        case "rock" :
-            return 'rock';
-            break;
-        case "paper" :
-            return 'paper';
-            break;
-        case "scissors" :
-            return 'scissors' ;
-            break;
-        default :
-            return "Unknown"
+    if (playerChoice === "rock") {
+        return 'rock';
+    } else if (playerChoice === "paper") {
+        return 'paper';
+    } else if (playerChoice === "scissors") {
+        return 'scissors';
+    } else if (playerChoice === "atom bomb") {
+        return 'atom bomb';
+    } else {
+        return "Not a valid value";
     }
-    return playerChoice;
 }
 
 function getComputerChoice() {
     const randomNumber = Math.random();
+    const absoluteUnit = Math.floor(randomNumber * (3 - 1 + 1)) + 1;
 
-    const absoluteUnit = Math.floor(randomNumber * (3-1+1))+1;
-
-    switch (absoluteUnit) {
-        case 1 :
-            return "rock";
-            break;
-        case 2 :
-            return "paper";
-            break;
-        case 3 :
-            return "scissors";
-            break;
-        default:
-            return "Unknown";
+    if (absoluteUnit === 1) {
+        return "rock";
+    } else if (absoluteUnit === 2) {
+        return "paper";
+    } else if (absoluteUnit === 3) {
+        return "scissors";
+    } else {
+        return "Unknown";
     }
-    return absoluteUnit;
 }
 
-function playRound (getComputerChoice, getPlayerChoice) {
-
-    if (getComputerChoice === getPlayerChoice) {
-        return "It's a tie!";
+function playRound(getComputerChoice, getPlayerChoice) {
+    if (getPlayerChoice === 'atom bomb') {
+        return "You cheater, atom bomb always wins!";
     } else if (
-        (getComputerChoice === "rock" && getPlayerChoice === "scissors" ) ||
-        (getComputerChoice === "paper" && getPlayerChoice === "rock" ) ||
-        (getComputerChoice === "scissors" && getPlayerChoice === "paper" )
+        (getComputerChoice === "rock" && getPlayerChoice === "scissors") ||
+        (getComputerChoice === "paper" && getPlayerChoice === "rock") ||
+        (getComputerChoice === "scissors" && getPlayerChoice === "paper")
     ) {
         return "The computer wins";
-    } else {
+    } else if (
+        (getPlayerChoice === "rock" && getComputerChoice === "scissors") ||
+        (getPlayerChoice === "paper" && getComputerChoice === "rock") ||
+        (getPlayerChoice === "scissors" && getComputerChoice === "paper")
+    ) {
         return "You win!";
+    } else {
+        return "It's a tie!";
     }
 }
+
 
 let player = getPlayerChoice();
 let computer = getComputerChoice();
 
-console.log(computer);
-console.log(player);
+console.log('computer: ' + computer);
+console.log('player: ' + player);
 
 console.log(playRound(computer,player));
